f4f5730876c0db5284f894ac2e39ac6678733896
Sanjay Saatyaki <Saatyaki.Sanjay@in.bosch.com>
2022-01-23 10:50:06 +0530

    Implmentation of Failure Test Cases

--- misaligned.py
+++ misaligned.py
@@ -1,13 +1,21 @@
 
+from sqlalchemy import all_
+
+
 def print_color_map():
+    all_color_codes = []
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
             print(f'{i * 5 + j} | {major} | {minor}')
-    return len(major_colors) * len(minor_colors)
+            all_color_codes.append(f'{i * 5 + j} | {major} | {minor}')
+    return len(major_colors) * len(minor_colors),all_color_codes
 
 
-result = print_color_map()
+result,all_color_codes = print_color_map()
 assert(result == 25)
+assert('White' in  all_color_codes[0])
+assert('Blue' in  all_color_codes[0])
+assert('1' in  all_color_codes[0])
 print("All is well (maybe!)\n")
