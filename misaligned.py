8442770b9bbb42e7ddf0d8dad9f3e7fc307748ce
Sanjay Saatyaki <Saatyaki.Sanjay@in.bosch.com>
2022-01-31 11:02:04 +0530

    Fix in Implementation

--- misaligned.py
+++ misaligned.py
@@ -1,20 +1,17 @@
+def get_pair_number(major_num,minor_num):
+    return (major_num * 5 + minor_num)+1
+
+def format_row(pair_number,major_colour, minor_color):
+    return '{pair_number}\t{major_colour}\t{minor_color}'
+
 def print_color_map():
-    all_color_codes = []
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
-            all_color_codes.append(f'{i * 5 + j} | {major} | {minor}')
-    return len(major_colors) * len(minor_colors),all_color_codes
+            print(format_row(get_pair_number(i,j),major,minor))
 
 
-result,all_color_codes = print_color_map()
-assert(result == 25)
-assert('White' in  all_color_codes[0])
-assert('Blue' in  all_color_codes[0])
-assert('1' in  all_color_codes[0])
-index_of_seperator = all_color_codes[0].index("|")
-for color in all_color_codes:
-    assert(index_of_seperator == color.index("|"))
+assert(get_pair_number(0,0)==1)
+assert(format_row(1,'White','Blue'=='1\tWhite\tBlue'))
 print("All is well (maybe!)\n")
